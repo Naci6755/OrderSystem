20e56822f44906eb5d5add59bc48c177db019899
naal3135 <naal3135@bht-berlin.de>
2025-11-28 02:38:24 +0100

    reduceStock test2

--- src/test/java/de/naci/ordermanagement/ProductTest.java
+++ src/test/java/de/naci/ordermanagement/ProductTest.java
@@ -1,6 +1,7 @@
 package de.naci.ordermanagement;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -8,10 +9,16 @@ import org.junit.jupiter.api.Test;
 public class ProductTest {
     
     @Test
-    @Order(1)
+    @Order(1)  //normalfall
     void testReduceStock() throws OutOfStockException{
         Product p1 = new Product("Handy", 123, 500.00, 50);
         p1.reduceStock(20);
         assertEquals(p1.getStockQuantity(), 30);
+    }
+    @Test
+    @Order(2)
+     void testReduceStockErr() throws OutOfStockException{
+        Product p1 = new Product("Handy", 123, 500.00, 50);
+        assertThrows(OutOfStockException.class, ()->{p1.reduceStock(70);});
     }
 }
