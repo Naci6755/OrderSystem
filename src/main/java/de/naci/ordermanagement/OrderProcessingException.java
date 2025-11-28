975b50e016fb1e641ea06c201a5e719398faa658
naal3135 <naal3135@bht-berlin.de>
2025-11-28 02:11:58 +0100

    mvn project setup + 2 Exception klassen

---
+++ src/main/java/de/naci/ordermanagement/OrderProcessingException.java
@@ -0,0 +1,7 @@
+package de.naci.ordermanagement;
+
+public class OrderProcessingException extends Exception{
+    public OrderProcessingException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/de/naci/ordermanagement/OutOfStockException.java
@@ -0,0 +1,6 @@
+package de.naci.ordermanagement;
+public class OutOfStockException extends Exception{
+   public OutOfStockException(String message){
+     super(message);
+   }
+}
